392b6bdda6bfe89026e8eb4eeb3f127c253bc327
VSRathore <vishan.singh23@gmail.com>
2015-08-20 14:23:04 +0530

    First Update

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return '<h1>Hello </h1>'
+
 @app.route('/<adds>')
 def main(adds):
     return '<h1>Hello User, %s!</h1>' % adds
